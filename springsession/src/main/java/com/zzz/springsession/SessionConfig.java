c4bbaec7ec119854c026d702270f163f7d1c3946
sKyanfIre <911930036@qq.com>
2019-09-25 01:07:58 +0800

    20190925->增加日志

--- springsession/src/main/java/com/zzz/springsession/SessionConfig.java
+++ springsession/src/main/java/com/zzz/springsession/SessionConfig.java
@@ -2,12 +2,14 @@ package com.zzz.springsession;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 /**
  *session过期时间1800s
  *
  */
+@Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 public class SessionConfig {
     @Value("${spring.redis.port:6379}")
--- springsession/src/main/java/com/zzz/springsession/controller/SessionController.java
+++ springsession/src/main/java/com/zzz/springsession/controller/SessionController.java
@@ -1,5 +1,7 @@
 package com.zzz.springsession.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,16 +16,22 @@ import java.util.Map;
  */
 @RestController
 public class SessionController {
+    private final static Logger logger = LoggerFactory.getLogger(SessionController.class);
     @RequestMapping(value = "/session/create",method = RequestMethod.POST)
     public Map<String,String> createSession(@RequestBody Map<String,String> request,HttpSession session){
+        logger.info("请求报文：" + request.toString());
        session.setAttribute(request.get("name"),request.get("value"));
+       logger.info("session=" + session.getId());
        return null;
     }
     @RequestMapping(value = "/session/get",method = RequestMethod.POST)
     public Map<String,String> getSessionAttribute(@RequestBody Map<String,String> request,HttpSession session){
+        logger.info("请求报文：" + request.toString());
         String value = session.getAttribute(request.get("name")) == null ? "" : session.getAttribute(request.get("name")).toString();
         Map<String,String> response = new HashMap<>(1);
         response.put(request.get("name"),value);
+        logger.info("session=" + session.getId());
+        logger.info("返回报文：" + response.toString());
         return response;
     }
 }
