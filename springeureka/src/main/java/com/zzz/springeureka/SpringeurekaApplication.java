150961e922f5a09713fea055561c467a4d2dc50f
zzz <911930036@qq.com>
2019-09-25 18:45:13 +0800

    20190926->整合apollo、新增eureka服务

---
+++ springeureka/src/main/java/com/zzz/springeureka/SpringeurekaApplication.java
@@ -0,0 +1,13 @@
+package com.zzz.springeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringeurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringeurekaApplication.class, args);
+	}
+
+}
